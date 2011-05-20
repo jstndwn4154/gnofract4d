b7c2210bba7cc7e34a9f9a11b878a9a48162b8dd
dx-mon <dx-mon@users.sourceforge.net>
2011-05-20 18:44:54 +0100

    Fixed an error with my_install_egg_info.py

--- buildtools/my_install_egg_info.py
+++ buildtools/my_install_egg_info.py
@@ -12,11 +12,9 @@ def chowner(dummy, dirpath, filelist):
 		os.chown(os.path.join(dirpath, file))
 
 class my_install_egg_info(install_egg_info):
-	def __init__(self, dict):
-		install_egg_info.__init__(self,dict)
-
 	def run(self):
-		install_egg_info.run()
-		if 'win' != sys.platform[:3]:
+		install_egg_info.run(self)
+		if ('win' != sys.platform[:3]):
 			base = 'debian/gnofract4d'
-			os.path.walk(base, chowner, None)
+			if (os.path.exists(base)):
+				os.path.walk(base, chowner, None)
