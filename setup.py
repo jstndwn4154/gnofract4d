a8103d3e3527dec1496030ab837944db79cfa155
dx-mon <dx-mon@users.sourceforge.net>
2013-03-30 01:27:39 +0000

    Improved the loop that figures out what translations should be coppied into the install

--- setup.py
+++ setup.py
@@ -280,10 +280,12 @@ def get_files(dir,ext):
 	return [ os.path.join(dir,x) for x in os.listdir(dir) if x.endswith(ext)] 
 
 mo = []
-for lang in os.listdir('build/locale'):
-	langDir = os.path.join('share', 'locale', lang, 'LC_MESSAGES')
-	langFile = os.path.join('build', 'locale', lang, 'LC_MESSAGES', 'gnofract4d.mo')
-	mo.append((langDir, [langFile]))
+for po in os.listdir('po'):
+	if (po.endswith('.po')):
+		lang = po[:-3]
+		langDir = os.path.join('share', 'locale', lang, 'LC_MESSAGES')
+		langFile = os.path.join('build', 'locale', lang, 'LC_MESSAGES', 'gnofract4d.mo')
+		mo.append((langDir, [langFile]))
 
 setup (name = 'gnofract4d',
 	   version = gnofract4d_version,
